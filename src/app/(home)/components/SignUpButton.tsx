d11fe50fc87af4db44eb6241f06d8fb7ba5a0047
Dejan Bogatinovski <bogatinovski.dejan@gmail.com>
2025-07-11 16:10:13 +0800

    Fix signup button

--- src/app/(home)/components/SignUpButton.tsx
+++ src/app/(home)/components/SignUpButton.tsx
@@ -1,15 +1,10 @@
-"use client";
-
 import { Button } from "@/app/(shared)/components/ui/button";
 import Link from "next/link";
 
 export default function SignUpButton() {
+  const signupLink = `${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/signup?response_type=code&client_id=${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI}&scope=openid+email+phone+profile`;
   return (
-    <Link
-      href={
-        "https://auth.nextlevelbundle.com/signup?response_type=code&client_id=2nrhrl7qnj55s275u2jd0ks9a1&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fcognito&scope=openid+email+phone&code_challenge=Naq-tRpO-rVBS3sNbGTaUyg_zmebJdlwJEahSera2O0&code_challenge_method=S256"
-      }
-    >
+    <Link href={signupLink}>
       <Button className="bg-primary text-white hover:bg-primary/90">
         Sign Up
       </Button>
