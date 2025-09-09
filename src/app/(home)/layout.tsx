a7cbf81f25afecc0bb21fc80fc3e7c3358ed6b93
Nenad Manev <nenad.manev@gmail.com>
2025-09-09 15:49:31 +0200

    fix layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,5 +1,7 @@
+
 import { getServerSession } from "@/lib/auth/server-auth";
 import CartProvider from "../(shared)/contexts/cart/cart-provider";
+import { Navigation } from "./components/navigation";
 
 export default async function RootLayout({
   children,
@@ -9,9 +11,12 @@ export default async function RootLayout({
   await getServerSession();
 
   return (
-    <main className="min-h-screen bg-background relative">
-      <div className="absolute inset-0 bg-mesh opacity-20 dark:opacity-10" />
-      <CartProvider initialCart={null}>{children}</CartProvider>
-    </main>
+    <CartProvider initialCart={null}>
+      <Navigation />
+      <main className="min-h-screen bg-background relative pt-16">
+        <div className="absolute inset-0 bg-mesh opacity-20 dark:opacity-10" />
+        {children}
+      </main>
+    </CartProvider>
   );
 }
