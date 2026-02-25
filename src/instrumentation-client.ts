7f2cd4f342365fa6a3f6bad63fc12e51c197df83
Dejan Bogatinovski <bogatinovski.dejan@gmail.com>
2026-02-25 13:56:40 +0100

    Update instrumentation-client.ts

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -7,6 +7,18 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://366d9bad0e6e69b841705c3398b4b755@o4510346714546176.ingest.us.sentry.io/4510346715201536",
 
+  // Ignore errors from browser extensions and third-party scripts
+  ignoreErrors: [
+    "SteamClient is not defined",
+    /Extensions\//, // Browser extension errors
+  ],
+  denyUrls: [
+    /\.millennium\//, // Steam Millennium mod
+    /extensions\//i, // Browser extensions
+    /^chrome-extension:\/\//, // Chrome extensions
+    /^moz-extension:\/\//, // Firefox extensions
+  ],
+
   // Add optional integrations for additional features
   integrations: [
     Sentry.replayIntegration(),
